08d05a5f5c70adf6cd0ac317525e721e03fda13e
LeonardoSoaresNunes <leonardoeaf2009@hotmail.com>
2021-06-15 17:05:35 -0300

    Corrigindo Erros de Programação

--- game.js
+++ game.js
@@ -3,4 +3,14 @@
 
 let board = ['','','','','','','','',''];
 let playerTime = 0;
-let simbols = ['0' , 'x'];
+let symbols = ['0' , 'x'];
+
+function handleMove(position){
+    board[position] = symbols[playerTime];
+
+    if (playerTime == 0){
+        playerTime = 1;
+    }else{
+        playerTime = 0;
+    }
+}
--- interface.js
+++ interface.js
@@ -1,12 +1,31 @@
-document.addEventListener('DomcontentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
     let pecas = document.querySelectorAll(".pecas");
 
-    pecas.forEach((pecas)=>{
+    pecas.forEach((pecas) => {
         pecas.addEventListener ('click', handleClick);
         })
     })
 
     function handleClick(event){
         console.log(event.target);
+
+        let pecas = event.target;
+        let position = pecas.id;
+
+        handleMove(position);
+        updatePecas();
+    }
+
+    function updatePecas(){
+        let pecas = document.querySelectorAll(".pecas");
+
+        pecas.forEach((pecas) => {
+            let position = pecas.id;
+            let symbol = board[position];
+
+            if(symbols != ''){
+                pecas.innerHTML = <div class='${symbols}'></div>
+            }
+        })
     }
